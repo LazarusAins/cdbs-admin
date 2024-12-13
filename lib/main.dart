b622200efb26a34960f112226c8cc08c58d0891b
paul@vontos.co <paul@vontos.co>
2024-12-13 13:02:58 +0800

    update web build

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,7 @@ import 'package:cdbs_admin/subpages/login_page.dart';
 import 'package:cdbs_admin/subpages/s1.dart';*/
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 /*import 'subpages/home_page.dart';
 import 'subpages/login_page.dart';
 import 'subpages/forgot_password.dart';
