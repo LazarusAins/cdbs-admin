57cb1fb0e6febdc2ffb6cf351a787cb48f7552e4
paul@vontos.co <paul@vontos.co>
2024-12-16 09:02:35 +0800

    update web build

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"a18df97ca57a249df5d8d68cd0820600223ce2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2743129681"
+    serviceWorkerVersion: "827570135"
   }
 });
