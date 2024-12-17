e439c53bd36bcaa2af93587c9bf366d28f04bd81
unknown <johnmark@vontos.co>
2024-12-17 16:27:30 +0800

    grade leve changes

--- lib/subpages/admissions/admission_schedules_page.dart
+++ lib/subpages/admissions/admission_schedules_page.dart
@@ -366,20 +366,12 @@ TextField(
       const SizedBox(height: 8),
       DropdownButtonFormField<String>(
         items: [
-          'Pre-Kinder',
-          'Kinder',
+          'Pre-Kinder & Kinder',
           'Grade 1',
           'Grade 2',
           'Grade 3',
-          'Grade 4',
-          'Grade 5',
-          'Grade 6',
-          'Grade 7',
-          'Grade 8',
-          'Grade 9',
-          'Grade 10',
-          'Grade 11',
-          'Grade 12',
+          'Grade 4 - 6',
+          'Grade 7 - 12',
         ]
         .map((grade) => DropdownMenuItem(
               value: grade,
