96c88bc810d22317a0cbe3e3a5fbeb21ccf8f595
paul@vontos.co <paul@vontos.co>
2025-02-03 15:24:09 +0800

    update admissions_schedule_page add new schedule prevent to add data when one of the controllers is empty

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"e672b006cb34c921db85b8e2f482ed3144a457
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2986051478"
+    serviceWorkerVersion: "1550290506"
   }
 });
